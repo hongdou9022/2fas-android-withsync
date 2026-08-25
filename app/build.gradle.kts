b0aa328134709f171936d5f9883a23fb4a6a81dc
hongdou9022 <862863826@qq.com>
2026-08-25 18:02:00 +0800

    发布 5.5.3-sync.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
 
     defaultConfig {
         applicationId = "com.cloud.twoasapp"
-        versionName = "5.5.3-sync.1"
-        versionCode = 5000035
+        versionName = "5.5.3-sync.2"
+        versionCode = 5000036
     }
 
     ksp {
